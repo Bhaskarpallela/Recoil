90f27cd47f6bf8e15b30c3d4d5584f50b7c24f4c
Bhaskarpallela <bhaskarpallela123@gmail.com>
2024-12-04 17:50:52 +0530

    This is first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+const countState = atom({
+  key: 'countState',
+  default: 0,
+});
+
+const Counter = () => {
+  const [count, setCount] = useRecoilState(countState);
+
+  return (
+    <div>
+      <h1 style={{color:'white'}}>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/store/atoms/counter.jsx
@@ -0,0 +1,7 @@
+
+import { atom } from "recoil"
+const Countatom=atom({
+key:'counter',
+default:0
+})
+export default Countatom;
